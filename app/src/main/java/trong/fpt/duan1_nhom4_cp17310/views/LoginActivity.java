ce09494e0e9dd46aae21d24d88bc91eda0b1e4ba
truong2408 <102342714+truong2408@users.noreply.github.com>
2022-11-02 10:31:08 +0700

    truong

--- app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/LoginActivity.java
+++ app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/LoginActivity.java
@@ -1,22 +1,36 @@
 package trong.fpt.duan1_nhom4_cp17310.views;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 
 import trong.fpt.duan1_nhom4_cp17310.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    GoogleSignInClient gsc;
+    GoogleSignInAccount account;
     private CheckBox chkShowPassword;
     private TextInputEditText tietPassword;
     private TextView tv_register;
@@ -50,5 +64,56 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //Đăng nhập google
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(LoginActivity.this, gso);
+        //Kiểm tra login Google
+        account = GoogleSignIn.getLastSignedInAccount(LoginActivity.this);
+        if (account != null) {
+            Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+
+        }
+
+        Button sib = findViewById(R.id.bt_gg);
+        sib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent googleIntent = gsc.getSignInIntent();
+                googleLauncher.launch(googleIntent);
+            }
+        });
     }
+
+    ActivityResultLauncher<Intent> googleLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent data = result.getData();
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    try {
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        String email = account.getEmail();
+                        String hoTen = account.getDisplayName();
+                        Log.d(">>>TAG", "onActivityResult: " + email);
+                        //Chuyển qua màn hình MainActivity
+                        if (account != null) {
+                            Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(homeIntent);
+                            finish();
+                        }
+                    } catch (Exception e) {
+                        Log.d(">>>TAG", "onActivityResult error: " + e.getMessage());
+                    }
+                }
+            }
+    );
 }
