3f548cb8dbd59469ba9fa01af1e9c543588d218d
truong2408 <102342714+truong2408@users.noreply.github.com>
2022-11-02 10:09:47 +0700

    truong

---
+++ app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/SplashActivity.java
@@ -0,0 +1,33 @@
+package trong.fpt.duan1_nhom4_cp17310.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import trong.fpt.duan1_nhom4_cp17310.R;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView iv_splash;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        iv_splash = findViewById(R.id.iv_splash);
+
+        Glide.with(this).load(R.drawable.ciname).into(iv_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+            }
+        },10000);
+    }
+}
