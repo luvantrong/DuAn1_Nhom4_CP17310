5e4c6c534e9d9a513fcbe791b5b64f08d3f9b2f5
luvantrong <102342647+luvantrong@users.noreply.github.com>
2022-11-02 21:06:28 +0700

    trong

--- app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/LoginActivity.java
+++ app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/LoginActivity.java
@@ -83,7 +83,7 @@ public class LoginActivity extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, Tesst.class);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/MainActivity.java
+++ app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/MainActivity.java
@@ -1,9 +1,11 @@
 package trong.fpt.duan1_nhom4_cp17310.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import trong.fpt.duan1_nhom4_cp17310.Fragment.FragmentTrangChu;
 import trong.fpt.duan1_nhom4_cp17310.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTrangChu fragmentTrangChu = new FragmentTrangChu();
+
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                .add(R.id.fr_main, fragmentTrangChu)
+                .commit();
     }
 }
