7296a185add8237e508954f7d693b8b08adf3c3d
luvantrong <102342647+luvantrong@users.noreply.github.com>
2022-11-05 22:22:09 +0700

    Trong

--- app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/RegisterActivity.java
+++ app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/RegisterActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import trong.fpt.duan1_nhom4_cp17310.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
