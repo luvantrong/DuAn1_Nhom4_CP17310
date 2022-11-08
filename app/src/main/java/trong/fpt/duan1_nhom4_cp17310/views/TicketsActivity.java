bd9ac99f8b7fb8fb15413387acf38cf621257743
truong2408 <102342714+truong2408@users.noreply.github.com>
2022-11-08 22:08:45 +0700

    truong

---
+++ app/src/main/java/trong/fpt/duan1_nhom4_cp17310/views/TicketsActivity.java
@@ -0,0 +1,16 @@
+package trong.fpt.duan1_nhom4_cp17310.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import trong.fpt.duan1_nhom4_cp17310.R;
+
+public class TicketsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tickets);
+    }
+}
